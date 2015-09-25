dd1ecb984dddcc1ba7c590bd326e3680effbcb51
oguratakayuki <otn.ogura@gmail.com>
2015-09-25 15:52:53 +0900

    last fmのメソッド移動

--- app/models/concerns/lastfm_api.rb
+++ app/models/concerns/lastfm_api.rb
@@ -5,10 +5,26 @@ module LastfmApi
   extend ActiveSupport::Concern
   included do
     attr_accessor :lastfm
-    @api_key = Settings.lastfm[:api_key]
-    @api_secret = Settings.lastfm[:api_secret]
-    @lastfm = Lastfm.new(@api_key, @api_secret)
-    @token = @lastfm.auth.get_token
+    def lastfm
+      api_key = Settings.lastfm[:api_key]
+      api_secret = Settings.lastfm[:api_secret]
+      @lastfm = Lastfm.new(api_key, api_secret)
+    end
+    def find_image
+      if mbid && image.url.nil?
+        api_result = self.search_from_api_by_mbid(mbid)
+        self.remote_image_url = api_result["image"].find{|t| t["size"] == "medium"}["content"]
+        save
+      end
+    end
+    def search_from_api_by_mbid(mbid)
+      lastfm.artist.get_info(mbid: mbid)
+    end
+
+    def search_from_api_by_name(name)
+      artists = lastfm.artist.search(artist: name)
+      artists["results"]["artistmatches"]["artist"].find{|t| URI.unescape(t["name"]).downcase == name }
+    end
   end
   module ClassMethods
     def find_or_create_by(param)
@@ -19,30 +35,20 @@ module LastfmApi
       else
         ret ||= find_by_mbid(mbid)
       end
-      if ret.present?
-        return ret
-      end
+      return ret if ret.present?
+      create_by_api(:lastfm)
+      artist
+    end
+    def create_by_api
       artist = Artist.new(name: name, mbid: mbid)
       request_hash = name ? {artist: name} : {mbid: mbid}
-      api_result = search_from_api(request_hash)
+      api_result = artist.search_from_api(request_hash)
       if api_result.present?
         artist.name = URI.unescape(api_result["name"]).downcase
         artist.mbid = api_result['mbid']
         #artist.remote_image_url = api_result["image"].find{|t| t["size"] == "medium"}["content"]
       end
       artist.save
-      artist
-    end
-    def search_from_api(request_hash)
-      artists = @lastfm.artist.search(request_hash)
-      temp = artists["results"]["artistmatches"]["artist"].find do |t|
-        if request_hash.key?(:artist)
-          URI.unescape(t["name"]).downcase == request_hash[:artist]
-        else
-          t["mbid"] == request_hash[:mbid]
-        end
-      end
-      temp
     end
   end
 end
